51c1284476717e0e06738149e4414bf56fcdaab2
Steven-Chavez <schavez256@yahoo.com>
2022-12-02 00:45:39 +0000

    Created ip calculator tool file

---
+++ Go/ip-checker.go
@@ -0,0 +1,15 @@
+// Author: Steven Chavez
+// FileName: ip-calculator.go
+// Creation Date: 12/1/2022
+
+package main
+
+import "fmt"
+
+func publicOrPrivateIP(ip string) {
+
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
