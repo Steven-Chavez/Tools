1a0194c184050765d4c1ab188a9f19604c2716da
Steven-Chavez <schavez256@yahoo.com>
2022-12-02 02:40:19 +0000

    Added logic to function

--- Go/ip-calculator.go
+++ Go/ip-calculator.go
@@ -6,15 +6,35 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func breakIntoOctets(ip string) {
-	split := strings.Split(ip, ".")
-	fmt.Println(split[0])
-	fmt.Println(split[1])
-	fmt.Println(split[2])
-	fmt.Println(split[3])
+func breakIntoOctets(ip string) [4]int {
+
+	var octet_str []string
+
+	var octet_int [4]int
+	var oct1 string
+	var oct2 string
+	var oct3 string
+	var oct4 string
+
+	octet_str = strings.Split(ip, ".")
+
+	oct1 = octet_str[0]
+	oct2 = octet_str[1]
+	oct3 = octet_str[2]
+	oct4 = octet_str[3]
+
+	octet_int[0], err = strconv.Atoi(oct1)
+	octet_int[1], err = strconv.Atoi(oct2)
+	octet_int[2], err = strconv.Atoi(oct3)
+	octet_int[3], err = strconv.Atoi(oct4)
+
+	fmt.Println(err)
+
+	return octet_int
 }
 
 // FUNCTION
@@ -28,5 +48,6 @@ func main() {
 	var ip string
 
 	fmt.Scanln(&ip)
-	breakIntoOctets(ip)
+	split := breakIntoOctets(ip)
+	fmt.Println(split)
 }
