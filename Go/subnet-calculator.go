48386399a088794cfcc883da910e7115a6182eaa
Steven-Chavez <schavez256@yahoo.com>
2022-12-09 01:26:17 +0000

    Added notes of function to create

--- Go/subnet-calculator.go
+++ Go/subnet-calculator.go
@@ -6,6 +6,17 @@ package main
 
 import "fmt"
 
+// CREATE FUNCTIONS FOR EACH TASK
+// -sanitizer
+// -spliter
+// -CIDR
+// -IP's in Range
+// -CIDR IP Range
+// -Subnet Mask
+func sanitizeInput() {
+	fmt.Println("Sanitizing")
+}
+
 func main() {
-	fmt.Println("vim-go")
+	sanitizeInput()
 }
